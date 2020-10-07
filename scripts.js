c70962bff833ec4d87e28a18d783ece08fa7ea7f
alecdalelio <alecdalelio@gmail.com>
2020-10-06 22:36:12 -0400

    updated JS and made responsive

--- scripts.js
+++ scripts.js
@@ -19,12 +19,97 @@ const game = ()=> {
         const options = document.querySelectorAll(".options button")
         const playerHand = document.querySelector(".player-hand");
         const computerHand = document.querySelector(".computer-hand");
+        const hands = document.querySelectorAll('.hands img');
+
+        hands.forEach(hand =>{
+            hand.addEventListener("animationend",function(){
+                this.style.animation = "";
+            })
+        })
+
         //Computer Options
         const computerOptions = ["rock", "paper", "scissors"];
-        const computerOptions = Math.random() * 3;
-        console.log(computerNumber);
-    }
 
+        options.forEach(option => {
+            option.addEventListener("click", function() {
+                //Computer choice
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+                
+                setTimeout(() =>{
+                //Here is where we call compare hands
+                compareHands(this.textContent, computerChoice);
+                
+                //Update images
+                playerHand.src = `./images/${this.textContent}.png`;
+                computerHand.src = `./images/${computerChoice}.png`;
+                }, 2000)
+
+                //Animation
+                playerHand.style.animation = "shakePlayer 2s ease";
+                computerHand.style.animation = "shakeComputer 2s ease";
+            });
+        });  
+    };
+
+const updateScore = () => {
+    const playerScore = document.querySelector('.player-score p');
+    const computerScore = document.querySelector('.computer-score p');
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+}
+
+const compareHands = (playerChoice, computerChoice) => {
+    //Update text
+    const winner = document.querySelector(".winner");
+    //Checking for a tie
+    if(playerChoice === computerChoice) {
+        winner.textContent = "It is a tie";
+        return;
+    }
+    //Check for rock
+    if(playerChoice === "rock"){
+        if(computerChoice === "scissors"){
+            winner.textContent= "Player wins!";
+            pScore++;
+            updateScore();
+            return;
+        } else {
+            winner.textContent= "Computer wins!";
+            cScore++;
+            updateScore();
+            return;
+        }
+    }
+    //Check for paper
+    if(playerChoice === "paper"){
+        if(computerChoice === "scissors"){
+            winner.textContent= "Computer wins!";
+            cScore++;
+            updateScore();
+            return;
+        } else {
+            winner.textContent= "Player wins!";
+            pScore++;
+            updateScore();
+            return;
+        }
+    }
+    // Check for scissors
+    if(playerChoice === "scissors"){
+        if(computerChoice === "rock"){
+            winner.textContent= "Computer wins!"
+            cScore++;
+            updateScore();
+            return;
+        } else {
+            winner.textContent= "Player wins!"
+            pScore++;
+            updateScore();
+            return;
+        }
+    }
+}
 // Call all the inner function
  startGame();
  playMatch();
